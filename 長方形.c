faa71db060a7f14b3a29f3186a7abf274fd6a81d
Yunyuhu <55233573+Yunyuhu@users.noreply.github.com>
2022-05-03 11:25:51 +0800

    Update 長方形.c

--- 長方形.c
+++ 長方形.c
@@ -1,17 +1,17 @@
-// #include<stdio.h>
-// void triangle();
-// int main(){
-//   triangle();
-//   return 0;
-// }
-// void triangle(){
-//   int width, height;
+#include<stdio.h>
+void triangle();
+int main(){
+  triangle();
+  return 0;
+}
+void triangle(){
+  int width, height;
 
-//   printf("寬：");
-//   scanf("%d",&width);
-//   printf("長：");
-//   scanf("%d",&height);
+  printf("寬：");
+  scanf("%d",&width);
+  printf("長：");
+  scanf("%d",&height);
 
-//   double total=width*height;
-//   printf("%.2f",total);
-// }
+  double total=width*height;
+  printf("%.2f",total);
+}
