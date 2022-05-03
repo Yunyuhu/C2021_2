42dec624073e7e506be846cbbe68a37d473d25e0
Yunyuhu <sandyfuoo68@gmail.com>
2022-05-03 11:14:31 +0800

    main

---
+++ 長方形.c
@@ -0,0 +1,17 @@
+// #include<stdio.h>
+// void triangle();
+// int main(){
+//   triangle();
+//   return 0;
+// }
+// void triangle(){
+//   int width, height;
+
+//   printf("寬：");
+//   scanf("%d",&width);
+//   printf("長：");
+//   scanf("%d",&height);
+
+//   double total=width*height;
+//   printf("%.2f",total);
+// }
---
+++ 階層.c
@@ -0,0 +1,18 @@
+// #include <stdio.h>
+// int num;
+// int factorial();
+// int main(){
+//   printf("please input a number:");
+//   scanf("%d",&num);
+  
+//   printf("factorial(%d)=%d",num,factorial(num));
+//   return 0;
+// }
+// int factorial(num){
+//   double total=1;
+//   int i;
+//   for(i=1;i<=num;i++){
+//     total*=i;
+//   }
+//   return total;
+// }
