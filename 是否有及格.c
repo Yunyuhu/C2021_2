e8cc896bb20bbeecbd80086cfd2cc6de12c25f0e
Yunyuhu <55233573+Yunyuhu@users.noreply.github.com>
2022-05-03 11:27:45 +0800

    Update 是否有及格.c

--- 是否有及格.c
+++ 是否有及格.c
@@ -1,15 +1,15 @@
-// #include <stdio.h>
-// void calender();
-// int main(void) {
-//   calender();
-//   return 0;
-// }
-// void calender(){
-//   int num;
-//   printf("輸入整數");
-//   scanf("%d",&num);
-//   if (num>=60)
-//   printf("pass");
-//   else
-//   printf("down");
-// }
+#include <stdio.h>
+void calender();
+int main(void) {
+  calender();
+  return 0;
+}
+void calender(){
+  int num;
+  printf("輸入整數");
+  scanf("%d",&num);
+  if (num>=60)
+  printf("pass");
+  else
+  printf("down");
+}
