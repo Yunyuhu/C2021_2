ac325e66dc79f27d8fbad5ea61339b1b7de265b9
Yunyuhu <55233573+Yunyuhu@users.noreply.github.com>
2022-05-03 11:16:13 +0800

    Update 三角.c

--- 三角.c
+++ 三角.c
@@ -1,16 +1,16 @@
-// #include <stdio.h>
-// int main(){
-//   struct tiangle{
-//     int bottom,height;
-//     double area;
-//   };
-//   struct tiangle tri;
+#include <stdio.h>
+int main(){
+   struct tiangle{
+     int bottom,height;
+     double area;
+   };
+   struct tiangle tri;
 
-//   printf("輸入三角形的長：");
-//   scanf("%d",&tri.bottom);
-//   printf("輸入三角形的寬：");
-//   scanf("%d",&tri.height);
-//   tri.area=(tri.bottom)*(tri.height)/2;
-//   printf("三角形面積為:%.2f",tri.area);
-//   return 0;
-// }
+   printf("輸入三角形的長：");
+   scanf("%d",&tri.bottom);
+   printf("輸入三角形的寬：");
+   scanf("%d",&tri.height);
+   tri.area=(tri.bottom)*(tri.height)/2;
+   printf("三角形面積為:%.2f",tri.area);
+   return 0;
+ }
